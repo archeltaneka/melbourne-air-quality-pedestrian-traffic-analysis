37180749561b6dc9d651500246475afb61eed66f
Archel Taneka <archeltaneka@gmail.com>
2025-12-11 13:23:49 +1100

    [Improvement] Merged downloading and processing data into one instead of 2 individual processes

--- data.py
+++ data.py
@@ -4,11 +4,15 @@ from pathlib import Path
 import pandas as pd
 
 from src.air_quality import AirQualityProcessor
+from src.downloader import Downloader
 from src.pedestrian_count import PedestrianCountProcessor
 
 
 if __name__ == '__main__':
     DATA_DIR = Path('data')
+
+    data_downloader = Downloader()
+    data_downloader.download()
 
     air_quality_processor = AirQualityProcessor()
     air_quality_df = pd.read_excel(DATA_DIR/'air_quality/2022_air_quality_vic.xlsx', sheet_name='AllData')
@@ -19,3 +23,7 @@ if __name__ == '__main__':
     pedestrian_dfs = [pd.read_csv(file) for file in pedestrian_files]
     pedestrian_df = pd.concat(pedestrian_dfs)
     pedestrian_df = pedestrian_processor.transform(pedestrian_df)
+
+    print(air_quality_df.head())
+    print(pedestrian_df.head())
+
--- download_data.py
+++
@@ -1,7 +0,0 @@
-from src.downloader import Downloader
-
-
-if __name__ == '__main__':
-    data_downloader = Downloader()
-    data_downloader.download()
-
