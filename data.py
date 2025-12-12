dda97269bd19cb91b288dc8a3d07185b0dc27cea
Archel Taneka <archeltaneka@gmail.com>
2025-12-12 21:08:54 +1100

    [Improvement] Combine download, preprocess, and map geolocations into a single script instead

--- data.py
+++ data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 
 from src.air_quality import AirQualityProcessor
+from src.area_mapping import AreaMapper
 from src.downloader import Downloader
 from src.pedestrian_count import PedestrianCountProcessor
 
@@ -11,18 +12,26 @@ from src.pedestrian_count import PedestrianCountProcessor
 if __name__ == '__main__':
     DATA_DIR = Path('data')
 
+    # Download air quality and pedestrian activity data
     data_downloader = Downloader()
     data_downloader.download()
 
+    # Preprocess air quality data
     air_quality_processor = AirQualityProcessor()
     air_quality_df = pd.read_excel(DATA_DIR/'air_quality/2022_air_quality_vic.xlsx', sheet_name='AllData')
     air_quality_df = air_quality_processor.transform(air_quality_df)
     air_quality_processor.save_data(air_quality_df, DATA_DIR/'air_quality/air_quality_final.csv')
 
+    # Preprocess pedestrian data
     pedestrian_processor = PedestrianCountProcessor()
-    pedestrian_files = glob(str(DATA_DIR/'pedestrian/*.csv'))
+    pedestrian_files = glob(str(DATA_DIR/'pedestrian/*_pedestrian_level.csv'))
     pedestrian_dfs = [pd.read_csv(file) for file in pedestrian_files if file != 'pedestrian_count_final.csv']
     pedestrian_df = pd.concat(pedestrian_dfs)
     pedestrian_df = pedestrian_processor.transform(pedestrian_df)
     pedestrian_processor.save_data(pedestrian_df, DATA_DIR/'pedestrian/pedestrian_count_final.csv')
-    
+
+    # Create and save area and latlong mapping
+    location_df = pedestrian_df[['nominatim_area']].copy()
+    location_df = location_df.drop_duplicates(subset=["nominatim_area"])
+    area_mapper = AreaMapper()
+    area_latlong_list = area_mapper.map_area_coordinates(location_df)
